80b99b26d60487d3af0cc27d2ac21f66a8a94ccf
alikeli <vita.ivanova92@mail.ru>
2022-12-17 21:49:02 +0300

    add commands

--- telegram-bot/src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ telegram-bot/src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -2,16 +2,22 @@ package pro.sky.telegrambot.listener;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.BotCommand;
 import com.pengrad.telegrambot.model.Update;
 
+import com.pengrad.telegrambot.model.botcommandscope.BotCommandScopeDefault;
+import com.pengrad.telegrambot.request.SetMyCommands;
 import com.pengrad.telegrambot.response.SendResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import pro.sky.telegrambot.entity.NotificationTask;
 import pro.sky.telegrambot.repository.NotificationTaskRepository;
 import pro.sky.telegrambot.service.NotificationService;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -27,6 +33,17 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         this.notificationTaskRepository = notificationTaskRepository;
         this.telegramBot = telegramBot;
         this.notificationService = notificationService;
+        List<BotCommand> listofCommands = new ArrayList<>();
+        listofCommands.add(new BotCommand("/start", "get a welcome message"));
+        listofCommands.add(new BotCommand("/deletemessage", "delete my message"));
+        listofCommands.add(new BotCommand("/help", "info how to use this bot"));
+
+//        try {
+//            this.execute(new SetMyCommands(listofCommands, new BotCommandScopeDefault(), null));
+//        } catch (TelegramApiException e) {
+//            log.error("Error setting bot's command list :" + e.getMessage());
+//        }
+
     }
 
     @PostConstruct
@@ -70,33 +87,33 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 //    }
 
 
-    @Scheduled(cron = "0 0/1 * * * *")
-    //repeat once a minute
-    public int notifies() {
-        // сначала проверяет имеются ли в БД напоминания на эту минуту - вернет список напоминаний,
-        // если такие есть
-        List<NotificationsTask> notificationsTaskList = notificationService.checkCurrentNotifications();
-        // если список непустой - вызывается метод makeNotification, который возвращает List<SendMessage>
-        // который несет id чата (кому это сообщение нужно отправить) и текст сообщения,
-        // который нужно отправить
-        if (!notificationsList.isEmpty()) {
-            notificationService.makeNotification(notificationsList)
-                    .forEach(n -> {
-                        // передаю SendMessage в .execute() - метод = сообщение отправлено в нужный чат
-                        SendResponse response = telegramBot.execute(n);
-                        System.out.println(response.isOk());
-                        System.out.println(response.errorCode());
-                    });
-            logger.info("выполнился метод notifies");
-        }
-        return UpdatesListener.CONFIRMED_UPDATES_ALL;
-    }
+//    @Scheduled(cron = "0 0/1 * * * *")
+//    //repeat once a minute
+//    public int notifies() {
+//        // сначала проверяет имеются ли в БД напоминания на эту минуту - вернет список напоминаний,
+//        // если такие есть
+//        List<NotificationTask> notificationsTaskList = notificationService.checkCurrentNotification();
+//        // если список непустой - вызывается метод makeNotification, который возвращает List<SendMessage>
+//        // который несет id чата (кому это сообщение нужно отправить) и текст сообщения,
+//        // который нужно отправить
+//        if (!notificationList.isEmpty()) {
+//            notificationService.makeNotification(notificationsList)
+//                    .forEach(n -> {
+//                        // передаю SendMessage в .execute() - метод = сообщение отправлено в нужный чат
+//                        SendResponse response = telegramBot.execute(n);
+//                        System.out.println(response.isOk());
+//                        System.out.println(response.errorCode());
+//                    });
+//            log.info("выполнился метод notifies");
+//        }
+//        return UpdatesListener.CONFIRMED_UPDATES_ALL;
+//    }
 
     @Scheduled(cron = "0 43 17 * * *")
     // метод удаляет устаревшие напоминания из БД
     public void deleteOldNotifications() {
         notificationService.deleteOldNotification();
-        logger.info("выполнился метод deleteOldNotifications - устаревшие напоминания удалены");
+        log.info("выполнился метод deleteOldNotifications - устаревшие напоминания удалены");
     }
 
 
--- telegram-bot/src/main/java/pro/sky/telegrambot/service/NotificationService.java
+++ telegram-bot/src/main/java/pro/sky/telegrambot/service/NotificationService.java
@@ -5,11 +5,14 @@ import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.request.SendMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import pro.sky.telegrambot.entity.NotificationTask;
 import pro.sky.telegrambot.repository.NotificationTaskRepository;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -22,6 +25,7 @@ public class NotificationService {
     public NotificationService(TelegramBot telegramBot, NotificationTaskRepository notificationsRepository) {
         this.telegramBot = telegramBot;
         this.notificationsRepository = notificationsRepository;
+
     }
     public void startCommandReceived(long chatId, String name) {
         String answer = "Hi, " + name + "! Nice to meet you!";
@@ -48,6 +52,21 @@ public class NotificationService {
         SendMessage message = new SendMessage(chatId, textToSend);
         telegramBot.execute(message);
 
+    }
+
+    // метод удаляет устаревшие напоминания, своего рода сборщик мусора
+    public void deleteOldNotification() {
+        // определяю дату и время, ранее которых напоминания считаю устаревшими.
+        LocalDateTime yesterday = LocalDateTime.now().minusDays(1);
+        // создаю временный список напоминаний, куда копирую только те напоминания,
+        // дата и время которых позже yesterday
+        List<NotificationTask> temporaryListNotifications = notificationsRepository.findAll().stream()
+                .filter(n -> n.getDateToSend().isAfter(yesterday))
+                .collect(Collectors.toList());
+        // удаляю все элементы из БД
+        notificationsRepository.deleteAll();
+        // сохраняю в БД все эелементы из временного списка
+        notificationsRepository.saveAll(temporaryListNotifications);
     }
 
 }
